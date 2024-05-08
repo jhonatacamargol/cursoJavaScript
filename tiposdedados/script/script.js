82b86561cccd9280fffd6b48187008271012d23f
jhonatacamargol <jhonatapires32@hotmail.com>
2024-05-08 10:21:39 -0300

    novo commit

---
+++ tiposdedados/script/script.js
@@ -0,0 +1,37 @@
+//string
+var nome = "João Ribeiro";
+var apelido = "Carinha";
+
+//número
+var a = 25;
+var b = 80.5;
+var c = 4.25e+6;
+
+//infinity
+console.log(100/0);
+
+//NaN - not a number
+console.log(0/0);
+
+//Bolean
+var administrador = false;
+var operador = true;
+
+//undefined
+var valorindefinido
+console.log(valorindefinido);
+
+//NULL
+var valor1 = '';
+var valor2 = 0;
+var valor3 = null;
+
+console.log(valor1);
+console.log(valor2);
+console.log(valor3);
+
+// verificar variáveis
+console.log(typeof(nome));
+console.log(typeof(b));
+console.log(typeof(administrador));
+console.log(typeof(valor3));
