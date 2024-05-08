d055f4ff468fe703e2b3b8a4b761c63303574823
jhonatacamargol <jhonatapires32@hotmail.com>
2024-05-08 09:25:11 -0300

    variaveis

---
+++ sintaxe/script/script.js
@@ -0,0 +1,38 @@
+console.log('instrução passo 1');
+console.log('instrução passo 2');
+
+//comentário de js
+/*esse comentário 
+possui mais linhas*/
+
+console.log(1);
+console.log(2);
+console.log(3);
+
+//variaveis
+var nome = "João José";
+var idade = 21;
+var administrador = false;
+
+var cidade
+cidade = "Lisboa";
+
+console.log(nome);//string
+console.log(idade);//number
+console.log(administrador);//booleano
+
+console.log(cidade);
+
+var valor1 = 100, nome2 = "Ana", apelido = "Silva";
+console.log(valor1,nome2,apelido);
+
+//let e const
+let texto = "frase de m texto";
+const pi = 3.14;
+texto ="nada";
+//pi = 25; não é possível atribuir outro valor a uma const
+
+//-----------------------------//------------------------//
+var nome = 'João Ribeiro';
+//alert(nome);
+document.write(nome);
